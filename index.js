6059ba3a9525fc13938137f6b2f9951822011a92
Bchan1999 <brandon.nic99@gmail.com>
2022-09-11 21:43:54 -0600

    Added comments & /items path

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+//NOTE: "nodemon index.js" script is used for start command. Listens
+//for any changes
+
+//packages
 const PORT = 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
 
+//creates an express application ex: app.use
 const app = express()
+const products = []
 
+//homepage path
 app.get('/', (req,res) => {
     res.json('Welcome to my Etsy API')
 })
@@ -14,8 +21,19 @@ app.get('/items', (req,res) => {
     axios.get('https://www.etsy.com/ca/shop/TheNeedleNanny?ref=simple-shop-header-name&listing_id=1159865656')
     .then((response) => {
         const html = response.data
-        console.log(html)
-    })
+        const $ = cheerio.load(html)
+        $('a.listing-link', html).each(function () {
+            const item  = $(this).attr('title')
+            const url =$(this).attr('href')
+            products.push({
+                item,
+                url
+            })
+
+        })
+        res.json(products)
+    }).catch((err) => console.log(err))
 })
 
+//check to make sure backend is working
 app.listen(PORT, () => console.log(`${PORT}`))
